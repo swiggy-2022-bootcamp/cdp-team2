61b4f99f7cbde9449fd1823b91fdaa6e3aa522bc
sachin som <sachinsom507@gmail.com>
2022-04-09 21:13:18 +0530

    (products-admin-ms): Added Swagger docs.

--- products-admin-service/internal/handlers/products.admin.go
+++ products-admin-service/internal/handlers/products.admin.go
@@ -15,6 +15,14 @@ func NewHandlers() *HandlersImpl {
 	return &HandlersImpl{}
 }
 
+// Health godoc
+// @Summary      Health Check Route
+// @Description  API to check products-admin health
+// @Tags         Health
+// @Accept       json
+// @Produce      plain/text
+// @Success      200  {object} 	string
+// @Router       / [get]
 func (h *HandlersImpl) Health(gctx *gin.Context) {
 	contentType := "text/plain; charset=utf-8"
 	gctx.Data(http.StatusOK, contentType, []byte(literals.HEALTH_MESSAGE))
