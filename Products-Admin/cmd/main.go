0edb7a71ac893dd09e43d172b53867ab40268321
sachin som <sachinsom507@gmail.com>
2022-04-14 12:03:16 +0530

    (products-admin-ms): Injected Dependencies in main file.

--- Products-Admin/cmd/main.go
+++ Products-Admin/cmd/main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/products-admin-service/internal/core/ports"
+	"github.com/products-admin-service/internal/core/services"
 	"github.com/products-admin-service/internal/handlers"
+	"github.com/products-admin-service/internal/repository"
 	"github.com/products-admin-service/internal/server"
 )
 
 var (
-	productsServer   ports.IServer
-	productsHandlers ports.IHandlers
+	err                error
+	productsServer     ports.IServer
+	productsHandlers   ports.IProdcutsHandlers
+	productsServices   ports.IProductsServices
+	productsRepository ports.IProductsRepository
 )
 
 func init() {
-	productsHandlers = handlers.NewHandlers()
+	// Load Dotenv file
+	if err = godotenv.Load(); err != nil {
+		log.Fatal("Error Loading in .env file: ", err.Error())
+	}
+
+	// InitialiZe Repository
+	// TODO: Need to load .env file via config.
+	productsRepository, err = repository.NewProductsRepository(os.Getenv("MONGO_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	productsRepository, _ = repository.NewProductsRepository("dummy")
+	productsServices = services.NewProductsServices(productsRepository)
+	productsHandlers = handlers.NewHandlers(productsServices)
 	productsServer = server.NewServer(productsHandlers)
 }
 
