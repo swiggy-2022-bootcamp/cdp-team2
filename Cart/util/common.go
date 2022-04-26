65a3e076b17f77c290d81943ea7fee76d9037cbd
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-04-26 13:38:16 +0530

    chore: remove debug logs

--- Cart/util/common.go
+++ Cart/util/common.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/dgrijalva/jwt-go/request"
-	log "github.com/sirupsen/logrus"
 	"github.com/swiggy-2022-bootcamp/cdp-team2/cart/config"
 )
 
@@ -18,10 +17,7 @@ func ExtractDetailsFromToken(req *http.Request) string {
 
 	// extract user id from the JWT token
 	claims := token.Claims.(jwt.MapClaims)
-	userInfo := claims["CustomUserInfo"].(map[string]interface{})
-
-	customerId := userInfo["_id"].(string)
-	log.Info(customerId)
+	customerId := claims["_id"].(string)
 
 	return customerId
 }
