d03ba94ee7cf02e4a4c38475d552f304f0790d51
Taranjeet <taranjeet.singh@trell.in>
2022-04-26 14:00:23 +0530

    [Chore]: Add aws dynamodb config + k8 node port for grpc calls

--- Orders/config/config.go
+++ Orders/config/config.go
@@ -3,8 +3,3 @@ package config
 var Server = map[string]string{
 	"PORT": "8002",
 }
-
-var AWS = map[string]string{
-	"region":   "us-west-2",
-	"endpoint": "http://localhost:8000",
-}
--- Shipping/config/config.go
+++ Shipping/config/config.go
@@ -4,11 +4,6 @@ var Server = map[string]string{
 	"PORT": "8003",
 }
 
-var AWS = map[string]string{
-	"region":   "us-west-2",
-	"endpoint": "http://localhost:8000",
-}
-
 var GrpcAdd = map[string]string{
 	"ORDER_SERVICE": "35.84.28.237:30208",
 }
