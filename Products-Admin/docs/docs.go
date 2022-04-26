727e8c75d1f828c2936302d9aff0063940a19468
sachin som <sachinsom507@gmail.com>
2022-04-26 06:06:52 +0530

    swagger path update

--- Products-Admin/docs/docs.go
+++ Products-Admin/docs/docs.go
@@ -677,7 +677,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8001",
+	Host:             "http:38.84.28.237:30201",
 	BasePath:         "/api/rest_admin/products/",
 	Schemes:          []string{},
 	Title:            "Swagger Products Admin Microservice",
