8cf106f3ad06b305308e8d25f28c2102a26cfcfe
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-04-26 15:53:47 +0530

    refactor: fix the customer id

--- Cart/internal/dao/dao.go
+++ Cart/internal/dao/dao.go
@@ -164,12 +164,12 @@ func (dao *dynamoDAO) GetCart(customerId string) (models.Cart, *errors.ServerErr
 	})
 
 	if err != nil {
-		cart.CustomerId = customerId
 		log.WithField("Error: ", err).Error("got error while calling GetItem API of dynamo db")
 		return cart, &errors.DatabaseQueryFailed
 	}
 
 	if result.Item == nil {
+		cart.CustomerId = customerId
 		errMsg := "Could not find cart for customer: '" + customerId + "'"
 		log.WithField("Error: ", errMsg).Error("record not found")
 		return cart, nil
